daf04d087c192ec120b078cb86f60ccb0777c912
aprende-devops <aprende-devops@users.noreply.github.com>
2021-11-16 18:25:10 +0000

    Increased version number [skip ci]

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2021031700;
+$plugin->version = 2021111600;
 $plugin->requires  = 2018051700; // Moodle 3.5.0
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'Version for Moodle 3.5 onwards';
